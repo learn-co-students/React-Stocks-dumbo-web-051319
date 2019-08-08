38bc0798201e64d72905342f6afb3dd1490939b1
NinjaEagle <kcwang06@gmail.com>
2019-08-08 10:48:42 -0400

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,27 +5,93 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  state = {
+    stocks: [],
+    portfolio: [],
+    filterTerm: "All",
+    sortTerm: ''
+  }
+  
+  componentDidMount(){
+    fetch('http://localhost:3000/stocks')
+    .then(res => res.json())
+    .then(jsonStocks => this.setState({
+      stocks: jsonStocks
+     
+    })
+    )
+  }
+  
+  setFilterTerm = (term) => {
+    this.setState({
+      filterTerm: term
+    })
+  }
+
+  setSortTerm = (term) => {
+  this.setState({
+    sortTerm:term
+  })}
+
+  buyStock = (stock) => {
+    this.setState((prevState) => {
+    return {
+      portfolio:[stock, ...prevState.portfolio]
+    }
+  })
+  }
+  removeStock = (stock) => {
+    let index = this.state.portfolio.indexOf(stock)
+    let stocks = [...this.state.portfolio]
+    stocks.splice(index,1)
+    this.setState({
+        portfolio: stocks
+    })
+  }
+
+  whichStockstoRender = () => {
+    let renderedStocks = [...this.state.stocks];
+    if(this.state.filterTerm === "All"){
+      renderedStocks = [...this.state.stocks]
+    }
+    else{
+      renderedStocks = this.state.stocks.filter(stock => stock.type === this.state.filterTerm)
+    }
+
+    if (this.state.sortTerm === "Price"){
+      renderedStocks.sort((a,b) => {
+        return a.price - b.price
+    })}
+    else if (this.state.sortTerm === "Alphabetically"){
+      renderedStocks.sort((a,b) => {
+        return a.name.localeCompare(b.name)
+      })
+   }
+    return renderedStocks
+  }
+
+  
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar setFilterTerm={this.setFilterTerm} term={this.state.filterTerm} setSortTerm={this.setSortTerm} sortTerm={this.state.sortTerm}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={this.whichStockstoRender()} buyStock ={this.buyStock}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer removeStock={this.removeStock} portfolio={this.state.portfolio}/>
 
             </div>
           </div>
       </div>
-    );
+    )
   }
 
 }
 
-export default MainContainer;
+export default MainContainer
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import Stock from '../components/Stock'
 
 class StockContainer extends Component {
+  showStocks = () => {
+    return this.props.stocks.map(stock => <Stock key={stock.id} stock={stock} handleClick={this.props.buyStock} />)
+  }
 
   render() {
     return (
       <div>
         <h2>Stocks</h2>
         {
+          this.showStocks()
           //render the list of stocks here
         }
       </div>
